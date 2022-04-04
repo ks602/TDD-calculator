c92939535f3bf9c8bd134031c8e5815d36e23534
ks602 <andywong12346@gmail.com>
2022-04-05 01:02:36 +0800

    init

---
+++ src/main/java/org/tdd/calculator.java
@@ -0,0 +1,7 @@
+package org.tdd;
+
+public class calculator {
+  public int add() {
+    return 0;
+  }
+}
---
+++ src/test/java/org/tdd/calculatorTest.java
@@ -0,0 +1,10 @@
+package org.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorTest {
+
+  @org.junit.jupiter.api.Test
+  void add() {
+  }
+}
