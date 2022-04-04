d227212caa04ed1f934e0718d0f1721365d784a7
ks602 <andywong12346@gmail.com>
2022-04-05 02:24:17 +0800

    Requirement 2

--- src/main/java/org/tdd/Calculator.java
+++ src/main/java/org/tdd/Calculator.java
@@ -1,8 +1,12 @@
 package org.tdd;
 
+import java.util.Arrays;
+
 public class Calculator {
   public int add(String testString) {
     String[] inputs = testString.split(",");
-    return Integer.parseInt(inputs[0]) + Integer.parseInt(inputs[1]);
+    return Arrays.stream(inputs)
+        .map(Integer::parseInt)
+        .reduce(0, Integer::sum);
   }
 }
--- src/test/java/org/tdd/calculatorTest.java
+++ src/test/java/org/tdd/calculatorTest.java
@@ -1,17 +1,36 @@
 package org.tdd;
 
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
 
   Calculator calculator = new Calculator();
 
-  @org.junit.jupiter.api.Test
+  @Test
   void testAddTwoNumbers() {
     String testString = "1,2";
 
     var result = calculator.add(testString);
 
     assertThat(result).isEqualTo(3);
+  }
+
+  @Test
+  void testAddMultipleNumbers() {
+    String testString = "1,2,3,4,5,6,7";
+
+    var result = calculator.add(testString);
+
+    assertThat(result).isEqualTo(28);
+  }
+
+  @Test
+  void testAddNegativeNumbers() {
+    String testString = "-5,5";
+
+    var result = calculator.add(testString);
+
+    assertThat(result).isEqualTo(0);
   }
 }
